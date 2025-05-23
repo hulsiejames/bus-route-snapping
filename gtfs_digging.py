a2bb01bbba8ac759527e914bbbf1384befb90f75
James Hulse <hulse.jjc@sky.com>
2025-05-23 18:32:02 +0100

    minor format updates

--- gtfs_digging.py
+++ gtfs_digging.py
@@ -13,8 +13,10 @@ import gpxpy
 import gpxpy.gpx
 from tqdm import tqdm
 
+
 def ct():
     return datetime.now().strftime("%H:%M:%S")
+
 
 GTFS_DIR = pathlib.Path(r"E:\Repos\bus-route-snapping\assets\gtfs\north_west")
 GPX_TRACES_DIR = pathlib.Path(r"E:\Repos\bus-route-snapping\assets\gpx_traces")
@@ -34,8 +36,10 @@ def create_gpx_files_from_csv(csv_path: pathlib.Path, output_dir: pathlib.Path):
 
     # Iterate over each row in the DataFrame
     for index, row in tqdm(df.iterrows(), desc="Creating GPX traces", total=len(df)):
-        trip_id = row['trip_id']
-        stop_coordinates = eval(row['stop_coordinate_array'])  # Convert string representation of list to actual list
+        trip_id = row["trip_id"]
+        stop_coordinates = eval(
+            row["stop_coordinate_array"]
+        )  # Convert string representation of list to actual list
 
         # Create a new GPX object
         gpx = gpxpy.gpx.GPX()
@@ -50,13 +54,14 @@ def create_gpx_files_from_csv(csv_path: pathlib.Path, output_dir: pathlib.Path):
 
         # Add points to the segment
         for lat, lon in stop_coordinates:
-            gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(latitude=lat, longitude=lon))
+            gpx_segment.points.append(
+                gpxpy.gpx.GPXTrackPoint(latitude=lat, longitude=lon)
+            )
 
         # Save the GPX file
         gpx_file_path = output_dir / f"{trip_id}_stop_trace.gpx"
-        with open(gpx_file_path, 'w') as f:
+        with open(gpx_file_path, "w") as f:
             f.write(gpx.to_xml())
-
 
 
 print("reading stop times")
@@ -69,31 +74,50 @@ print("reading stops")
 stops = pd.read_csv(GTFS_DIR / "stops.txt", low_memory=False)
 print("data read")
 
-st_test = stop_times.groupby("trip_id")
-
-departure_time = pd.to_datetime(DEPARTURE_TIME).time()
-departure_time = datetime.strptime(DEPARTURE_TIME, "%H:%M:%S")
-trip_end_time = departure_time + timedelta(minutes=TRIP_TIME_MINS)
+# st_test = stop_times.groupby("trip_id")
 
 # Convert departure_time column to datetime.time for comparison
 stop_times["departure_time"] = pd.to_datetime(
     stop_times["departure_time"],
     format="%H:%M:%S",
     errors="coerce",
-    ).dt.time
+).dt.time
 
+if DEPARTURE_TIME:
 
-i_len = len(stop_times)
-# Filter based on time window
-stop_times = stop_times[
-    (stop_times["departure_time"] >= departure_time.time()) &
-    (stop_times["departure_time"] <= trip_end_time.time())
-]
-print(f"Removed {i_len - len(stop_times):,} rows for being outside of the time window: {departure_time.time()} - {trip_end_time.time()}")
+    if not TRIP_TIME_MINS:
+        raise ValueError(
+            "`TRIP_TIME_MINS must be specified if `DEPARTURE_TIME` is not None"
+        )
+
+    departure_time = pd.to_datetime(DEPARTURE_TIME).time()
+    departure_time = datetime.strptime(DEPARTURE_TIME, "%H:%M:%S")
+    trip_end_time = departure_time + timedelta(minutes=TRIP_TIME_MINS)
+
+    print(f"Filtering trips for period: {departure_time} -> {trip_end_time}")
+
+    i_len = len(stop_times)
+    # Filter based on time window
+    stop_times = stop_times[
+        (stop_times["departure_time"] >= departure_time.time())
+        & (stop_times["departure_time"] <= trip_end_time.time())
+    ]
+    print(
+        f"Removed {i_len - len(stop_times):,} rows for being outside of the time window: {departure_time.time()} - {trip_end_time.time()}"
+    )
+
+# departure_time = pd.to_datetime(DEPARTURE_TIME).time()
+# departure_time = datetime.strptime(DEPARTURE_TIME, "%H:%M:%S")
+# trip_end_time = departure_time + timedelta(minutes=TRIP_TIME_MINS)
+
 
 # THings to investigate
-top_check = stop_times.loc[stop_times["trip_id"] == "VJd52ffba9e123b1a8d51fd4e3844e51e0d506f99f"].copy()
-top_check_pls_be_the_same = stop_times.loc[stop_times["trip_id"] == "VJf470303a637ffc221bcc1c814e0e788f2c883b44"].copy()
+top_check = stop_times.loc[
+    stop_times["trip_id"] == "VJd52ffba9e123b1a8d51fd4e3844e51e0d506f99f"
+].copy()
+top_check_pls_be_the_same = stop_times.loc[
+    stop_times["trip_id"] == "VJf470303a637ffc221bcc1c814e0e788f2c883b44"
+].copy()
 
 st_grp_test = stop_times.loc[stop_times.groupby("trip_id")["stop_sequence"].idxmax()]
 # No count in here should be above 1 if this has worked
@@ -101,10 +125,10 @@ st_grp_test["trip_id"].value_counts()
 
 
 # Max stop sequence SHOULD be 3 here.
-assure_st_grp_test = stop_times.loc[stop_times["trip_id"] == "VJffffe5f8fd900a3e8e9e64e4691e556ddaeed637"].copy()
+assure_st_grp_test = stop_times.loc[
+    stop_times["trip_id"] == "VJffffe5f8fd900a3e8e9e64e4691e556ddaeed637"
+].copy()
 # NB stop_sequence starts from 0 indexed stop - so above is acutally 4 stop route
-
-
 
 
 print(f"commencing merge {ct()}")
@@ -113,24 +137,32 @@ trip_stop_times = pd.merge(
     right=stop_times,
     left_on="trip_id",
     right_on="trip_id",
-    )
+)
 print(f"merge complete {ct()}")
-print(f"With {len(trip_stop_times):,} rows and {len(trip_stop_times['trip_id'].unique()):,} unique trip ids")
+print(
+    f"With {len(trip_stop_times):,} rows and {len(trip_stop_times['trip_id'].unique()):,} unique trip ids"
+)
 
 # Then apply groupby on route_id trip_id & idxmax on stop_sequence!
 print(f"commencing groupby {ct()}")
-trip_stop_times_grped = trip_stop_times.loc[trip_stop_times.groupby(["route_id", "trip_id"])["stop_sequence"].idxmax()].copy()
+trip_stop_times_grped = trip_stop_times.loc[
+    trip_stop_times.groupby(["route_id", "trip_id"])["stop_sequence"].idxmax()
+].copy()
 print(f"completed {ct()}")
-print(f"leaving {len(trip_stop_times_grped):,} rows and {trip_stop_times_grped['trip_id'].nunique():,} unique trip ids")
+print(
+    f"leaving {len(trip_stop_times_grped):,} rows and {trip_stop_times_grped['trip_id'].nunique():,} unique trip ids"
+)
 
 
 i_len = len(trip_stop_times)
-print(f"Rmoving trips with a stop departure time before departure_time {departure_time}")
+# bprint(
+#    f"Rmoving trips with a stop departure time before departure_time {departure_time}"
+# )
 # Now, iterate over every identified trip_id and identify it's stop sequence
 valid_trip_id = trip_stop_times.copy()
 # Filter rows where 'departure_time' is greater than or equal to DEPARTURE_TIME
-#valid_trip_id = valid_trip_id[valid_trip_id['departure_time'] >= DEPARTURE_TIME]
-#print(f"Removed {i_len - len(valid_trip_id):,} rows before {departure_time} ({len(valid_trip_id):,} remain)")
+# valid_trip_id = valid_trip_id[valid_trip_id['departure_time'] >= DEPARTURE_TIME]
+# print(f"Removed {i_len - len(valid_trip_id):,} rows before {departure_time} ({len(valid_trip_id):,} remain)")
 
 # Join on stops
 valid_trip_id = pd.merge(
@@ -140,7 +172,7 @@ valid_trip_id = pd.merge(
     right_on="stop_id",
     indicator=True,
     validate="m:1",
-    )
+)
 print(valid_trip_id["_merge"].value_counts())
 valid_trip_id = valid_trip_id.drop(columns=["_merge"])
 
@@ -148,55 +180,64 @@ valid_trip_id = valid_trip_id.drop(columns=["_merge"])
 trip_ids = list(valid_trip_id["trip_id"].unique())
 
 
-
 # Dictionary to store coordinates for each trip_id
-trip_coordinates = {}
-
+#
+#
 # =============================================================================
 # # Iterate over each trip_id
 # for trip_id in tqdm(trip_ids, desc="building coordinate arrays", total=len(trip_ids)):
 #     # Filter rows matching the current trip_id
 #     trip_data = valid_trip_id[valid_trip_id["trip_id"] == trip_id]
-# 
+#
 #     # Sort by stop_sequence
 #     trip_data = trip_data.sort_values(by="stop_sequence")
-# 
+#
 #     # Extract coordinates (stop_lat, stop_lon) in the order of stop_sequence
 #     coordinates = list(zip(trip_data["stop_lat"], trip_data["stop_lon"]))
-# 
+#
 #     # Store the coordinates in the dictionary
 #     trip_coordinates[trip_id] = coordinates
 # =============================================================================
 
-# Attempt 2 
+# Attempt 2
 grouped = valid_trip_id.groupby("trip_id")
 
+
+trip_coordinates = {}
 trip_data_dict = {}
 
-for trip_id, trip_data in tqdm(grouped, desc="building coordinate arrays", total=len(grouped)):
+for trip_id, trip_data in tqdm(
+    grouped, desc="building coordinate arrays", total=len(grouped)
+):
     trip_data = trip_data.sort_values("stop_sequence")
     stop_coordinates = list(zip(trip_data["stop_lat"], trip_data["stop_lon"]))
     trip_coordinates[trip_id] = stop_coordinates
     stop_ids = list(trip_data["stop_id"])
-    #stop_times = list(trip_data["departure_time"].dt.time())
+    # stop_times = list(trip_data["departure_time"].dt.time())
     # stop_times = [t.strftime("%H:%M:%S") for t in trip_data["departure_time"].dt.time]
-    stop_times = [pd.to_datetime(t, format="%H:%M:%S").strftime("%H:%M:%S") for t in trip_data["departure_time"]]
+    stop_times = [
+        pd.to_datetime(t, format="%H:%M:%S").strftime("%H:%M:%S")
+        for t in trip_data["departure_time"]
+    ]
 
-    
     trip_data_dict[trip_id] = {
         "stop_coordinate_array": stop_coordinates,
         "stop_ids": stop_ids,
-        "stop_times": stop_times
+        "stop_times": stop_times,
     }
 
-    
-df_trip_coordinates = pd.DataFrame(list(trip_coordinates.items()), columns=["trip_id", "stop_coordinate_array"])
-df_trip_data = pd.DataFrame([
-    {"trip_id": trip_id, **data} for trip_id, data in trip_data_dict.items()
-])
 
+df_trip_coordinates = pd.DataFrame(
+    list(trip_coordinates.items()), columns=["trip_id", "stop_coordinate_array"]
+)
+df_trip_data = pd.DataFrame(
+    [{"trip_id": trip_id, **data} for trip_id, data in trip_data_dict.items()]
+)
 
-time_period = f"{str(departure_time.time()).replace(':','-')}_{str(trip_end_time.time()).replace(':','-')}"
+if departure_time:
+    time_period = f"{str(departure_time.time()).replace(':','-')}_{str(trip_end_time.time()).replace(':','-')}"
+else:
+    time_period = "all_services"
 
 df_trip_coordinates.to_csv(f"{time_period}_trip_stop_times.csv", index=False)
 df_trip_data.to_csv(f"{time_period}_trip_data_stop_times.csv", index=False)
@@ -209,13 +250,13 @@ print(f"Creating GPX traces for {len(df_trip_data):,} trips")
 create_gpx_files_from_csv(
     csv_path=f"{time_period}_trip_data_stop_times.csv",
     output_dir=GPX_TRACES_DIR,
-    )
+)
 
 print("Process completed!")
 
 # =============================================================================
 # 025-05-22 17:23:02,579 - INFO - Route id: 29 has 27 unique associated trip_ids
-# 2025-05-22 17:23:02,580 - INFO - Unique trip_id counts per trip_id: 
+# 2025-05-22 17:23:02,580 - INFO - Unique trip_id counts per trip_id:
 # trip_id
 # VJd52ffba9e123b1a8d51fd4e3844e51e0d506f99f    1
 # VJf470303a637ffc221bcc1c814e0e788f2c883b44    1
@@ -248,11 +289,9 @@ print("Process completed!")
 # =============================================================================
 
 
-
-
 # =============================================================================
 # 025-05-22 17:17:01,205 - INFO - Route id: 103798 has 18 unique associated trip_ids
-# 2025-05-22 17:17:01,213 - INFO - Unique trip_id counts per trip_id: 
+# 2025-05-22 17:17:01,213 - INFO - Unique trip_id counts per trip_id:
 # trip_id
 # VJ3632947e4e409ed14b9db24d52c8915db23b767e    1
 # VJba552053579a8b08e780d2b6e1059b94c7c77ccd    1
@@ -272,5 +311,5 @@ print("Process completed!")
 # VJ0eebc3308eec7d0a3afd8f15d56a0dbd1d8e9e32    1
 # VJ4a1cc970b9c4045a2e61e05a08b39219f9b18250    1
 # VJce10856055b8e91a401341ca19f1e527cf33a10d    1
-# 
+#
 # =============================================================================
